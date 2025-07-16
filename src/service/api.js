dcce30e16ec990f99bc9d46a7f468b0c93a8b477
David Barragán Merino <david.barragan@kaleidos.net>
2025-07-16 13:28:33 +0200

    fix: remove console log

--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = import.meta.env.VUE_APP_API_URL
-
-console.log(import.meta.env)
-
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 axios.defaults.headers.common.Accept = 'application/json'
 
